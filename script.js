f4c0a3d4781424c618bdf2c9a9ad1a9478e3ef50
islandnate <pontenate@gmail.com>
2022-06-13 20:12:54 -0700

    added A search btn

--- script.js
+++ script.js
@@ -1,25 +1,51 @@
 // https://www.omdbapi.com/?i=tt3896198&apikey=954794aa
 
-const movieListEl = document.querySelector(".movies")
+const movieListEl = document.querySelector('.movies__container')
+const searchBar = document.querySelector('.search__bar')
+const searchBtn = document.querySelector('.btn__search')
+let searchResults = document.getElementsByClassName('.search__results')
+let moviesArr = []
 
 
-async function main(){
-    const movies = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=954794aa")
-    const moviesData = await movies.json()
-    console.log(moviesData)
-    movieListEl.innerHTML = moviesData.map((movie) => movieHTML(movie)).join("")
+async function main() {
+    
+    const movies = await fetch(`https://omdbapi.com/?apikey=d3ff36ad&s=movie`)
+    moviesData = await movies.json()
+    const moviesSearch = moviesData.Search
+    movieListEl.innerHTML = moviesSearch.map((movie) => movieHTML(movie)).join("")
+    
+    searchBar.addEventListener('keyup', async (e) => {
+            if(e.key === 'Enter'){
+                const searchString = e.target.value.toLowerCase()
+                const moviesNew = await fetch(`https://omdbapi.com/?apikey=d3ff36ad&s=${searchString}`)
+                moviesNewData = await moviesNew.json()
+                const moviesNewSearch = moviesNewData.Search
+                movieListEl.innerHTML = moviesNewSearch.map((movie) => movieHTML(movie)).join("")
 
+            }    
+    })       
+       
 }
-
 
 main()
 
+
+    searchBtn.addEventListener('Click', async (e) => {
+        if(e.key === 'Click'){
+            const searchString = e.target.value.toLowerCase()
+            const moviesNew = await fetch(`https://omdbapi.com/?apikey=d3ff36ad&s=${searchString}`)
+            moviesNewData = await moviesNew.json()
+            const moviesNewSearch = moviesNewData.Search
+            movieListEl.innerHTML = moviesNewSearch.map((movie) => movieHTML(movie)).join("")
+
+        }})   
+
 function movieHTML(movie){
     return `<div class="movie">
-    <img src="https://${movie.poster}" alt="" class="movie__img">
+    <img src="${movie.Poster}" alt="" class="movie__img">
     <div class="movie__description">
-        <div class="movie__title">${movie.title}</div>
-        <div class="movie__year"> ${movie.year}</div>
+        <div class="movie__title">${movie.Title}</div>
+        <div class="movie__year"> ${movie.Year}</div>
     </div>
 </div>`
 }
